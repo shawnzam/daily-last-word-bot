bdba2dd7f7a32ff150941149a9a7991cfa188b69
Zamechek, Shawn <zamechek@wharton.upenn.edu>
2015-04-17 08:35:34 -0400

    readme update

--- README.md
+++ README.md
@@ -16,3 +16,5 @@ You can run the bot on Node.js with the `node bot.js` commmand.
 * Simple.
 * easy.
 * useless.
+
+*This is a system that seems to have more failures than successes. Love is present with nearly all inmates until the end.*
--- bot.js
+++ bot.js
@@ -7,51 +7,55 @@ var T = new Twit(require('./config.js'));
 
 var lastwords = require('./lastwords.json');
 
+function getLastWord(){
+	var lastword = {};
+	while(Object.keys(lastword).length < 3){
+		var randomnumber=Math.floor(Math.random()*lastwords.length)
+		lastword = lastwords[randomnumber];	
+	}
+	var offender = lastword["Offender:"];
+	offender = offender.replace(/#\d*/, '').replace(/^\s+|\s+$/g, '');
+	return 'On ' + lastword['Date of Execution:'].replace(/^\s+|\s+$/g, '') + ' ' 
+	+ offender + ' had these last words: "' 
+	+ lastword['Last Statement:'].replace(/^\s+|\s+$/g, ''); + '"';
+}
+	
+function breakDownString(str, len){
+	var returnArray = []
+	if(str.length <= len){
+		return [str]
+	}
+	while(str.length >len){
+		var tmpString = str.substring(0,len);
+		if(tmpString.lastIndexOf(' ') <= 0){
+			returnArray.push(str.substring(0,len) + "...");
+			str = str.substring(len);
+		} else {
+			returnArray.push(str.substring(0,tmpString.lastIndexOf(' ')) + "...");
+			str = str.substring(tmpString.lastIndexOf(' '));
+		}
+	}
+	returnArray.push(str);
+	return returnArray;
+}
 
-function getLastWord() {
-    var lastword = {};
-    while (Object.keys(lastword).length < 3) {
-        var randomnumber = Math.floor(Math.random() * lastwords.length)
-        lastword = lastwords[randomnumber];
-    }
-    var offender = lastword["Offender:"];
-    offender = offender.replace(/#\d*/, '').replace(/^\s+|\s+$/g, '');
-    return 'On ' + lastword['Date of Execution:'].replace(/^\s+|\s+$/g, '') + ' ' + offender + ' had these last words: "' + lastword['Last Statement:'].replace(/^\s+|\s+$/g, ''); + '"';
+function go(){
+	var tweetAsArray = breakDownString(getLastWord(), 137);
+		async.eachSeries(tweetAsArray.reverse(), function(el, callback) {
+			T.post('statuses/update', { status: el }, function(err, data, response) {
+			if(!err){
+				console.log(el);
+				callback();
+			}
+		});
+	})
 }
 
 
-function breakDownString(str, len) {
-    var returnArray = []
-    if (str.length <= len) {
-        return [str]
-    }
-    while (str.length > len) {
-        var tmpString = str.substring(0, len);
-        if (tmpString.lastIndexOf(' ') <= 0) {
-            returnArray.push(str.substring(0, len) + "...");
-            str = str.substring(len);
-        } else {
-            returnArray.push(str.substring(0, tmpString.lastIndexOf(' ')) + "...");
-            str = str.substring(tmpString.lastIndexOf(' '));
-        }
-    }
-    returnArray.push(str);
-    return returnArray;
-}
-
-function go() {
-    var tweetAsArray = breakDownString(getLastWord(), 137);
-    async.eachSeries(tweetAsArray.reverse(), function(el, callback) {
-        T.post('statuses/update', {
-            status: el
-        }, function(err, data, response) {
-            if (!err) {
-                console.log(el);
-                callback();
-            }
-        });
-    })
+// setInterval(go(), 60000 * 60 * 24);
+go();
 
 
-    // setInterval(go(), 60000 * 60 * 24);
-    go();
+
+
+
